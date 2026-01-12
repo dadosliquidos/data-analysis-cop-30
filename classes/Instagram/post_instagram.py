8fda78dc69ad821716c43cf7d7a097441836c428
jo4o-gust4vo <j.gustavo.devs@gmail.com>
2026-01-12 11:43:52 -0300

    add metodos para inserir informações de cada post do instagram

---
+++ classes/Instagram/post_instagram.py
@@ -0,0 +1,24 @@
+
+
+
+
+class PostInstagram:
+    def __init__(self, link,data, fonte_jornal):
+        self.link = link
+        self.data = data
+        self.fonte_jornal =  fonte_jornal
+
+    
+    def set_link(self):
+        'link_do_post = self.link '
+        return self.link 
+
+
+    def set_data(self):
+        'data_post = self.data'
+        'realizar aqui tratamento de datas'
+        return self.data
+    
+    def set_fonte_jornal(self):
+        'fonte_do_jornal = self.fonte_jornal'
+        return self.fonte_jornal
