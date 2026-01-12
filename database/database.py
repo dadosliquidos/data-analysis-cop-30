8f3243a590712f71d7a872a93c1381c0c86e2cf2
jo4o-gust4vo <j.gustavo.devs@gmail.com>
2026-01-12 11:36:15 -0300

    criação com a conexao e confuguração e funcionalidades do banco de dados

---
+++ database/database.py
@@ -0,0 +1,69 @@
+import mysql.connector
+from mysql.connector import errorcode
+from dotenv import load_dotenv
+import pandas as pd
+import os
+
+
+load_dotenv()
+HOST = os.getenv('HOST')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+DATABASE =  os.getenv('DATABASE')
+
+
+#cursor = con.cursor()
+ 
+
+#cursor.execute('select * from comentarios')
+
+
+#for (id, comentario,sentimento) in cursor:
+#    print(id,comentario,sentimento)
+
+class DB:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def conexao():
+        con = mysql.connector.connect(
+                                    host=HOST, 
+                                    user=USER, 
+                                    password=PASSWORD, 
+                                    database=DATABASE)
+        
+        con.set_charset_collation(charset='utf8mb4',collation='utf8mb4_unicode_ci')
+
+        return con
+    
+    @classmethod
+    def insert_comment(cls, list_comment,data,fonte_jornal ):
+        con = DB.conexao()
+        cursor = con.cursor()
+        df = pd.DataFrame(list_comment,columns=['comentarios'])
+        df = df.replace('"', "'")
+        #df = pd.read_csv('comentarios.csv', encoding='utf-8',sep=';')
+        #df.columns = ['comentarios']
+        for comment in df['comentarios']:
+    
+            query = 'insert into comentarios (comentario,data_post,fonte_jornal) values("{}", "{}" , "{}")'.format(comment,data,fonte_jornal)
+            print(query)
+            cursor.execute(query)
+            con.commit()
+        
+        #cursor.close()
+        print('Comentários persistidos com sucesso.')  
+
+
+#DB.insert_comment(['comentari"o "1" ',"comentario 2"],'2001/11/22','Liberal')
+
+     
+
+
+
+
+
+
+
+
